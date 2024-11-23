d246b7d33eed46d2019bc1805e4a477203d7b217
Mark <markinoe@gmail.com>
2024-11-22 20:17:38 -0800

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -29,8 +29,10 @@ dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 client = pymongo.MongoClient(os.environ['MONGO_URL'])
 mydb = client.Cluster0
+
 mycol = mydb["transit_speed_data"]
 header_col = mydb["headers"]
+trip_id_col = mydb["trip_ids"]
 
 #from graphing import graph_variables
 #set active directory to file location
@@ -78,15 +80,15 @@ def get_headers_df():
 
 def get_trip_ids_df():
     myquery = {}
-    mydoc = mycol.find(myquery)
+    mydoc = trip_id_col.find(myquery)
     df = pd.DataFrame(list(mydoc))
     
     if df.empty:
-        df = pd.DataFrame(columns = ["Trip ID"])
-        df = pd.concat([df, pd.DataFrame([{"Trip ID": "Placeholder"}])], ignore_index=True)
+        df = pd.DataFrame(columns = ["Trip_ID_ID", "Trip_ID"])
+        df = pd.concat([df, pd.DataFrame([{"Trip_ID_ref": 0, "Trip_ID": 0}])], ignore_index=True)
     return df
 
-update_static()
+#update_static()
 
 trips = pd.read_csv("google_transit/trips.csv")
 routes = pd.read_csv("google_transit/routes.csv")
@@ -126,12 +128,12 @@ def snapshot():
                     header_ID = int(headers_df.loc[headers_df['Header'] == header]['Header_ID'].values[0])
                 else:
                     header_ID = int(headers_df['Header_ID'].max() + 1)
-                    new_header = {
+                    new_row = {
                         "Header_ID": header_ID,
                         "Header": header
                     }
 
-                    header_col.insert_one(new_header)
+                    header_col.insert_one(new_row)
                     headers_df = get_headers_df()
                 #check if trip_id is already in the trip_ids collection. If not, add it in and increment the tripID from the last tripID. If it is in, get the tripID and set trip_id to the tripID
                 #This scheme helps reduce storage space in the database
@@ -142,14 +144,16 @@ def snapshot():
                 trip_ID = int(trip_ID[:trip_ID.find(":")])
 
                 if trip_ID in trip_ids_df.values:
-                    trip_ID = int(trip_ids_df.loc[trip_ids_df['Trip ID'] == trip_ID]['Trip ID'].values[0])
+                    trip_ID = int(trip_ids_df.loc[trip_ids_df['Trip_ID'] == trip_ID]['Trip_ID_ref'].values[0])
+                
                 else:
-                    trip_ID = int(trip_ids_df['Trip ID'].max() + 1)
-                    new_trip_id = {
-                        "Trip ID": trip_ID
+                    trip_ID_ref = int(trip_ids_df["Trip_ID_ref"].max() + 1)
+                    new_row = {
+                        "Trip_ID_ref": trip_ID_ref,
+                        "Trip_ID": trip_ID
                     }
 
-                    mycol.insert_one(new_trip_id)
+                    trip_id_col.insert_one(new_row)
                     trip_ids_df = get_trip_ids_df()
                     
                 new_mongo_row = {
